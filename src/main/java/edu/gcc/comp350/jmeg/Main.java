6b89eb6e9b906d8f943421088548f12def361bd5
ethanplank <82603228+ethanplank@users.noreply.github.com>
2023-02-22 15:26:41 -0500

    Ethans commit

--- src/main/java/edu/gcc/comp350/jmeg/Main.java
+++ src/main/java/edu/gcc/comp350/jmeg/Main.java
@@ -2,6 +2,8 @@ package edu.gcc.comp350.jmeg;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Testing our first class!!! :)");
+        System.out.println("Ethan's print statement");
     }
 }
