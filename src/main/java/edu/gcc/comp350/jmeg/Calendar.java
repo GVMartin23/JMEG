6d33b9acd287a124056125035109667c6001ac63
ethanplank <82603228+ethanplank@users.noreply.github.com>
2023-03-30 14:11:23 -0400

    Making calendar visualization

--- src/main/java/edu/gcc/comp350/jmeg/Calendar.java
+++ src/main/java/edu/gcc/comp350/jmeg/Calendar.java
@@ -3,13 +3,57 @@ package edu.gcc.comp350.jmeg;
 public class Calendar {
     private Schedule currentSchedule;
 
-    public void Schedule(){
-
-    }
-    public void displayCalendar(){
-
+    public Calendar(Schedule currentSchedule){
+this.currentSchedule=currentSchedule;
     }
 
+    public String showCalendar(Schedule schedule){
+        String[][] calendarArray=new String[5][10];
+        calendarArray[0][0]="Monday\t\t";
+        calendarArray[1][0]="Tuesday\t\t";
+        calendarArray[2][0]="Wednesday\t";
+        calendarArray[3][0]="Thursday\t";
+        calendarArray[4][0]="Friday\t\t";
+
+        for(int k=0; k<currentSchedule.getCourses().size(); k++) {
+            String time = currentSchedule.getCourses().get(k).getBegin_tim();
+            time = Course.convertStringToTime(time);
+            String courseName = currentSchedule.getCourses().get(k).getCrs_title();
+            if (currentSchedule.getCourses().get(k).getMonday_cde().equals("M")) {//TODO change k
+                calendarArray[0][k+1] = time + " " + courseName;//[0][?] change from k+1
+            }
+            if (currentSchedule.getCourses().get(k).getTuesday_cde().equals("T")) {
+                calendarArray[1][k+1] = time + " " + courseName;
+            }
+            if (currentSchedule.getCourses().get(k).getWednesday_cde().equals("W")) {
+                calendarArray[2][k+1] = time + " " + courseName;
+            }
+            if (currentSchedule.getCourses().get(k).getThursday_cde().equals("R")) {
+                calendarArray[3][k+1] = time + " " + courseName;
+            }
+            if (currentSchedule.getCourses().get(k).getFriday_cde().equals("F")) {
+                calendarArray[4][k+1] = time + " " + courseName;
+            }
+        }
+
+            String string="";
+        for(int i=0; i<5; i++){
+            for(int j=0; j<10; j++){
+                if(calendarArray[i][j]==null){
+                    if(j>4){
+                        string+=(j-4)+"|";
+                    }else{
+                        string+=(j+8)+"|";
+                    }
+                }else {
+                    string += calendarArray[i][j] + "|";//Building the calendar string
+                }
+            }
+            string+="\n";
+        }
+        System.out.println(string);
+        return string;
+    }
     public Schedule getCurrentSchedule() {
         return currentSchedule;
     }
---
+++ src/test/java/edu/gcc/comp350/jmeg/CalendarTest.java
@@ -0,0 +1,32 @@
+package edu.gcc.comp350.jmeg;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalendarTest {
+
+    @Test
+    public void calendarTest(){
+        Main.testCSV();
+        ArrayList<Course> courses=Main.getCourses();
+
+        User u=new User("Ethan", "CS", "AI", 2024);
+        Course c= courses.get(176);//100);//new Course("Course1", "12/12/2000 08:00:00", "12/12/2000 09:00:00");
+        Course c2= courses.get(2);//176);// Course("Course2", "12/12/2000 12:00:00", "12/12/2000 02:00:00");
+        Course c3=courses.get(3);//12);
+       //
+        // System.out.prinln(c3.getCrs_title());
+        ArrayList<Course> fakeCourseList=new ArrayList<Course>();
+        fakeCourseList.add(c);
+        fakeCourseList.add(c2);
+        fakeCourseList.add(c3);
+        Schedule s=new Schedule(u, "Fake schedule", fakeCourseList);
+
+        Calendar cal=new Calendar(s);
+        cal.showCalendar(s);
+    }
+
+}
