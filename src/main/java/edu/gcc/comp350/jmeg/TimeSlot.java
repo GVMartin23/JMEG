5a79813d260bbd4ffea0870530d0ec2030e5fe7f
GVMartin23 <MartinGV19@gcc.edu>
2023-03-30 13:49:45 -0400

    Implementing timeslot so parse String times to int codes
    Added getters and setters

--- src/main/java/edu/gcc/comp350/jmeg/TimeSlot.java
+++ src/main/java/edu/gcc/comp350/jmeg/TimeSlot.java
@@ -1,10 +1,6 @@
 package edu.gcc.comp350.jmeg;
 
 public class TimeSlot {
-    private enum WeekDay {
-        MON, TUES, WED, THURS, FRI
-    }
-
     private boolean onMonday;
     private boolean onTuesday;
     private boolean onWednesday;
@@ -12,8 +8,72 @@ public class TimeSlot {
     private boolean onFriday;
     private boolean inPerson;
 
-    int[] timeCodes;
+    int[] beginTimeCodes;
+    int[] endTimeCodes;
 
+    public boolean isOnMonday() {
+        return onMonday;
+    }
+
+    public void setOnMonday(boolean onMonday) {
+        this.onMonday = onMonday;
+    }
+
+    public boolean isOnTuesday() {
+        return onTuesday;
+    }
+
+    public void setOnTuesday(boolean onTuesday) {
+        this.onTuesday = onTuesday;
+    }
+
+    public boolean isOnWednesday() {
+        return onWednesday;
+    }
+
+    public void setOnWednesday(boolean onWednesday) {
+        this.onWednesday = onWednesday;
+    }
+
+    public boolean isOnThursday() {
+        return onThursday;
+    }
+
+    public void setOnThursday(boolean onThursday) {
+        this.onThursday = onThursday;
+    }
+
+    public boolean isOnFriday() {
+        return onFriday;
+    }
+
+    public void setOnFriday(boolean onFriday) {
+        this.onFriday = onFriday;
+    }
+
+    public boolean isInPerson() {
+        return inPerson;
+    }
+
+    public void setInPerson(boolean inPerson) {
+        this.inPerson = inPerson;
+    }
+
+    public int[] getBeginTimeCodes() {
+        return beginTimeCodes;
+    }
+
+    public void setBeginTimeCodes(int[] beginTimeCodes) {
+        this.beginTimeCodes = beginTimeCodes;
+    }
+
+    public int[] getEndTimeCodes() {
+        return endTimeCodes;
+    }
+
+    public void setEndTimeCodes(int[] endTimeCodes) {
+        this.endTimeCodes = endTimeCodes;
+    }
 
     public TimeSlot (Course c) {
         onMonday = c.getMonday_cde().equals("M");
@@ -22,40 +82,64 @@ public class TimeSlot {
         onThursday = c.getThursday_cde().equals("R");
         onFriday = c.getFriday_cde().equals("F");
         inPerson = onMonday || onTuesday || onWednesday || onThursday || onFriday;
-        timeCodes = getTimeCodes(c);
+        beginTimeCodes = new int[5];
+        endTimeCodes = new int[5];
+        getTimeCodes(c);
     }
 
-    private int[] getTimeCodes(Course c) {
-        int[] timeCodes = {0,0,0,0,0}; //Init to all zeroes, change if has time code for specific day
+    private void getTimeCodes(Course c) {
         if (onMonday) {
-            timeCodes[0] = getCode(c.getBegin_tim(), c.getEnd_tim());
+            beginTimeCodes[0] = getCode(c.getBegin_tim());
+            endTimeCodes[0] = getCode(c.getEnd_tim());
         }
 
         if (onTuesday) {
-            timeCodes[1] = getCode(c.getBegin_tim(), c.getEnd_tim());
+            beginTimeCodes[1] = getCode(c.getBegin_tim());
+            endTimeCodes[1] = getCode(c.getEnd_tim());
         }
 
         if (onWednesday) {
-            timeCodes[2] = getCode(c.getBegin_tim(), c.getEnd_tim());
+            beginTimeCodes[2] = getCode(c.getBegin_tim());
+            endTimeCodes[2] = getCode(c.getEnd_tim());
         }
 
         if (onThursday) {
-            timeCodes[3] = getCode(c.getBegin_tim(), c.getEnd_tim());
+            beginTimeCodes[3] = getCode(c.getBegin_tim());
+            endTimeCodes[3] = getCode(c.getEnd_tim());
         }
 
         if (onFriday) {
-            timeCodes[4] = getCode(c.getBegin_tim(), c.getEnd_tim());
+            beginTimeCodes[4] = getCode(c.getBegin_tim());
+            endTimeCodes[4] = getCode(c.getEnd_tim());
         }
-
-
-
-
-        return timeCodes;
-
     }
 
-    private int getCode(String beginTime, String endTime) {
+    /**
+     * Converts string formatted as HH:MM:SS to number
+     * multiplies HH by 60 then add minutes
+     * @param timeString string to convert
+     * @return corresponding time
+     */
+    private int getCode(String timeString) {
+        String[] parseBeginTime = timeString.split(":");
 
-        return 0;
+        //Convert Hour to minutes
+        int time = convertToMilitary(Integer.parseInt(parseBeginTime[0])) * 60;
+        //Add minutes to
+        time += Integer.parseInt(parseBeginTime[1]);
+
+        return time;
     }
+
+    /**
+     * Converts hours 1,2,3,4,5,6,7 to military time
+     * Used so that they are comparable
+     * @param num num to convert
+     * @return if num < 8 (earliest classes) and not 0 (assigned to classes with no times)
+     * Then add 12 to number, Else return number
+     */
+    private int convertToMilitary(int num) {
+        return num < 8 && num != 0 ? num + 12 : num;
+    }
+
 }
