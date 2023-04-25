4f080bcd33fccec8641645eb67f4fb6fd813a57e
GVMartin23 <MartinGV19@gcc.edu>
2023-04-25 15:29:47 -0400

    Added creating schedule

--- src/main/java/edu/gcc/comp350/jmeg/ScheduleResponse.java
+++ src/main/java/edu/gcc/comp350/jmeg/ScheduleResponse.java
@@ -1,22 +1,50 @@
 package edu.gcc.comp350.jmeg;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ScheduleResponse {
 
-    @GetMapping("/schedule")
+    @GetMapping("/scheduleList")
     public ArrayList<Schedule> retrieveSchedules() {
         return Main.getSchedules();
     }
 
     @GetMapping("/schedulePick")
-    public void selectSchedule(@RequestParam(value = "title", defaultValue = "") String scheduleTitle) {
-        Main.setCurrentSchedule(Main.getSchedules().stream().filter(s -> s.getTitle().equals(scheduleTitle)).collect(Collectors.toList()).get(0));
+    public boolean selectSchedule(@RequestParam(value = "title", defaultValue = "") String scheduleTitle) {
+        List<Schedule> scheduleList = Main.getSchedules().stream().filter(s -> s.getTitle().equals(scheduleTitle)).collect(Collectors.toList());
+
+        if (scheduleList.size() == 0) {
+            return false;
+        }
+
+        Main.setCurrentSchedule(scheduleList.get(0));
+        return true;
+    }
+
+    @GetMapping("/scheduleCreate")
+    public boolean createSchedule(@RequestParam(value = "title", defaultValue = "") String scheduleTitle,
+                                  @RequestParam(value = "semester", defaultValue = "") String semester,
+                                  @RequestParam(value = "year", defaultValue = "") String year) {
+        if (scheduleTitle.isEmpty() || semester.isEmpty() || year.isEmpty()) {
+            return false;
+        }
+
+        try {
+            int yearVal = Integer.parseInt(year);
+            Schedule newSchedule = new Schedule(scheduleTitle, 0, semester, yearVal);
+            Main.getSchedules().add(newSchedule);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
