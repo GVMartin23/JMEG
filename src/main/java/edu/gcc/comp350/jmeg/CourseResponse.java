df37a88cae6c6d4b536935d338f8b53a61939989
GVMartin23 <MartinGV19@gcc.edu>
2023-04-27 16:02:51 -0400

    Minor fix

--- src/main/java/edu/gcc/comp350/jmeg/CourseResponse.java
+++ src/main/java/edu/gcc/comp350/jmeg/CourseResponse.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 @CrossOrigin(origins = "http://localhost:3000")
 public class CourseResponse {
 
-    @GetMapping("courseDetails")
+    @GetMapping("/courseDetails")
     public String getDetails(@RequestParam(value = "code", defaultValue = "")String code) {
         try {
             Schedule currentSchedule = Main.getCurrentSchedule();
